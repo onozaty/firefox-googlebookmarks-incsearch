601b4036966fac42f355a224bc3d4a2b33f7b544
onozaty <onozaty@gmail.com>
2013-07-14 15:39:23 +0900

    Added menu control.

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,11 @@
 const Cc = Components.classes;
 const Ci = Components.interfaces;
+
 var WindowWatcher  = Cc["@mozilla.org/embedcomp/window-watcher;1"].getService(Ci.nsIWindowWatcher);
 var WindowMediator = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
 var PromptService  = Cc["@mozilla.org/embedcomp/prompt-service;1"].getService(Ci.nsIPromptService);
 var PrefService  = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService);
-  
+
 var windowObserver = {
   observe: function (aSubject, aTopic, aData) {
 
@@ -25,61 +26,130 @@ var windowObserver = {
 
 function initializeWindow(aWindow) {
 
-  var NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+  initializeToolbar(aWindow);
 
-  // toolbar item
-  var paletteElem = aWindow.document.getElementById("BrowserToolbarPalette")
-                    || aWindow.document.getElementById("navigator-toolbox").palette;
+  if (aWindow.document.readyState == "complete") {
+    resetToolbar(aWindow);
+  }
 
-  var toolbarbutton = aWindow.document.createElementNS(NS, "toolbarbutton");
+  initializeShortcutKey(aWindow);
+  initializeMenu(aWindow);
+}
 
-  toolbarbutton.setAttribute("id", "googleBookmarksIncsearchButton");
+function finalizeWindow(aWindow) {
+
+  removeElement(aWindow, TOOLBAR_BUTTON_ELEMENT_ID);
+  removeElement(aWindow, SHORTCUT_KEY_ELEMENT_ID);
+  removeElement(aWindow, MENU_ITEM_ELEMENT_ID);
+}
+
+function removeElement(aWindow, id) {
+
+  var element = aWindow.document.getElementById(id);
+  if (element != null) { 
+    element.parentNode.removeChild(element);
+  }
+}
+
+
+// toolbar
+const TOOLBAR_BUTTON_ELEMENT_ID = "googleBookmarksIncsearchButton";
+
+function initializeToolbar(aWindow) {
+
+  var palette = aWindow.document.getElementById("BrowserToolbarPalette")
+                || aWindow.document.getElementById("navigator-toolbox").palette;
+
+  var toolbarbutton = aWindow.document.createElement("toolbarbutton");
+
+  toolbarbutton.setAttribute("id", TOOLBAR_BUTTON_ELEMENT_ID);
   toolbarbutton.setAttribute("label", "Google Bookmarks IncSearch");
   toolbarbutton.setAttribute("tooltiptext", "Google Bookmarks IncSearch");
   toolbarbutton.setAttribute("image", "chrome://googlebookmarks_incsearch/skin/icon-small.png");
 
-  toolbarbutton.addEventListener('command', function() {
+  toolbarbutton.addEventListener("command", function() {
     openIncSearch(aWindow);
   }, false);
 
-  paletteElem.appendChild(toolbarbutton);
+  palette.appendChild(toolbarbutton);
+}
 
-  // shortcut key
-  var prefBranch = PrefService.getBranch('extensions.googlebookmarks_incsearch.');
+function resetToolbar(aWindow) {
+
+  var navigatorToolbox = aWindow.document.getElementById("navigator-toolbox");
+  var toolbars = Array.prototype.slice.call(aWindow.document.getElementsByTagName("toolbar"));
+
+  toolbars.forEach(function (toolbar) {
+     if (toolbar.toolbox !== navigatorToolbox) {
+       return;
+     }
+
+     var curset = toolbar.getAttribute("currentset");
+     toolbar.currentSet = curset;
+  });
+}
+
+// shortcut key
+const SHORTCUT_KEY_ELEMENT_ID = "googleBookmarksIncsearchShortcutKey"
+
+function initializeShortcutKey(aWindow) {
+
+  var prefBranch = PrefService.getBranch("extensions.googlebookmarks_incsearch.");
 
   var modifiers = "accel";
   try {
-     modifiers = prefBranch.getCharPref('shortcut.open.modifiers');
+     modifiers = prefBranch.getCharPref("shortcut.open.modifiers");
   } catch(e) {}
 
   var key = ":"
   try {
-    key = prefBranch.getCharPref('shortcut.open.key');
+    key = prefBranch.getCharPref("shortcut.open.key");
   } catch(e) {}
 
   var isDisabledShortcutKey = false;
   try {
-    isDisabledShortcutKey = prefBranch.getBoolPref('shortcut.open.disabled');
+    isDisabledShortcutKey = prefBranch.getBoolPref("shortcut.open.disabled");
   } catch(e) {}
 
   if (!isDisabledShortcutKey) {
 
-    var mainKeysetElem = aWindow.document.getElementById("mainKeyset");
-    var keyElem = aWindow.document.createElementNS(NS, "key");
+    var mainKeyset = aWindow.document.getElementById("mainKeyset");
+    var keyElement = aWindow.document.createElement("key");
 
-    keyElem.setAttribute("id", "key_open_googleBookmarksIncsearch");
-    keyElem.setAttribute("key", key);
-    keyElem.setAttribute("modifiers", modifiers);
-    keyElem.setAttribute("oncommand", "void(0);");
+    keyElement.setAttribute("id", SHORTCUT_KEY_ELEMENT_ID);
+    keyElement.setAttribute("key", key);
+    keyElement.setAttribute("modifiers", modifiers);
+    keyElement.setAttribute("oncommand", "void(0);");
 
-    keyElem.addEventListener('command', function() {
+    keyElement.addEventListener("command", function() {
       openIncSearch(aWindow);
     }, false);
 
-    mainKeysetElem.appendChild(keyElem);
+    mainKeyset.appendChild(keyElement);
   }
-
 }
+
+// menu
+const MENU_ITEM_ELEMENT_ID = "googleBookmarksIncsearchMenuitem"
+
+function initializeMenu(aWindow) {
+
+  var menu = aWindow.document.getElementById("menu_ToolsPopup");
+
+  var menuitem = aWindow.document.createElement("menuitem");
+
+  menuitem.setAttribute("id", MENU_ITEM_ELEMENT_ID);
+  menuitem.setAttribute("label", "Google Bookmarks IncSearch");
+  menuitem.setAttribute("insertbefore", "sanitizeSeparator");
+  menuitem.setAttribute("key", SHORTCUT_KEY_ELEMENT_ID);
+
+  menuitem.addEventListener("command", function() {
+    openIncSearch(aWindow);
+  }, false);
+
+  menu.appendChild(menuitem);
+}
+
 
 function openIncSearch(aWindow) {
   var gBrowser = aWindow.gBrowser;
@@ -102,21 +172,6 @@ function openIncSearch(aWindow) {
     gBrowser.selectedTab = target;
     target.linkedBrowser.contentDocument.getElementById('text').focus();
   }
-}
-
-
-function finalizeWindow(aWindow) {
-
-  var toolbarbutton = aWindow.document.getElementById("googleBookmarksIncsearchButton")
-  toolbarbutton.parentNode.removeChild(toolbarbutton);
-
-  //PromptService.alert(null, "toolbarbutton", toolbarbutton);
-
-  var keyItem = aWindow.document.getElementById("key_open_googleBookmarksIncsearch")
-  if (keyItem != null) { 
-    keyItem.parentNode.removeChild(keyItem);
-  }
-
 }
 
 
